cfefe121b6c55ee598004e67d3bc0c799631af5f
leaf corcoran <leafot@gmail.com>
2025-02-27 13:07:53 -0800

    only install prereqs when not using cache

--- main.js
+++ main.js
@@ -19,16 +19,6 @@ const main = async () => {
 
   const extractPath = path.join(process.cwd(), BUILD_PREFIX, `openresty-${openrestyVersion}`)
 
-  // Install prerequisites
-  if (process.platform === 'linux') {
-    notice("Installing prerequisites for Linux")
-    await exec.exec('sudo apt-get install -q libpcre3-dev build-essential', undefined, {
-      env: {
-        DEBIAN_FRONTEND: "noninteractive",
-        TERM: "linux"
-      }
-    })
-  }
 
   const cachePaths = [BUILD_PREFIX]
   const cacheKey = makeCacheKey(openrestyVersion, configureFlags || "")
@@ -45,6 +35,17 @@ const main = async () => {
   }
 
   if (!restoredCache) {
+    // Install prerequisites
+    if (process.platform === 'linux') {
+      notice("Installing prerequisites for Linux")
+      await exec.exec('sudo apt-get install -q libpcre3-dev build-essential', undefined, {
+        env: {
+          DEBIAN_FRONTEND: "noninteractive",
+          TERM: "linux"
+        }
+      })
+    }
+
     const sourceTar = await tc.downloadTool(`https://openresty.org/download/openresty-${openrestyVersion}.tar.gz`)
 
     await io.mkdirP(extractPath)
